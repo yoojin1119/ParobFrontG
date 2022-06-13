e2e5cdbd8d10c85a30124a018656bc7354da1359
박유진 <bag-yujin@bag-yujin-ui-MacBookPro.local>
2022-06-13 17:46:44 +0900

    constants 파일 정리

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,3 +1,78 @@
-export const Color = {
+const Color = {
+    primaryYellow: '#FBC531',
+    primaryOrange: '#ED6F1E',
+    primaryRed: '#E83B3C',
+    primaryBlue: '#3FBDEE',
+    secondaryYellow: '#EDA311',
+    secondaryBrown: '#B84C17',
+    secondaryRed:'#C01717',
+    secondaryBule: '#0FA1CD',
+    secondaryGreen: '#64BD9E',
+    secondaryDeepGreeon: '#1D9B65',
+    textWhite:'#FFFFFF',
+    textDeepGray: '#3D3D3D',
+    textGray: '#7B7B7B',
+    textLightGray: 'B7B7B7',
+    textBlack: '#000000',
+    lineBlack:'#000000',
+    lineDeepGray: '#7B7B7B',
+    lineGray: '#B7B7B7',
+    lineLightGray: '#D3D3D3',
+    Select: '#FFF4D8',
+    BackGray: '#EDEDED',
+    BackLight: '#F9F9F9'
+}
 
+const Font = {
+    h1 :`font-family: IBM Plex Sans KR;
+    font-size: 36px;
+    font-weoght: bold;`,
+    h2 :`font-family: IBM Plex Sans KR;
+    font-size: 28px;
+    font-weoght: bold;`,
+    h3 :`font-family: IBM Plex Sans KR;
+    font-size: 20px;
+    font-weoght: 500;`,
+    h4 :`font-family: IBM Plex Sans KR;
+    font-size: 18px;
+    font-weoght: 400;`,
+    bigRegular :`font-family: IBM Plex Sans KR;
+    font-size: 16px;
+    font-weoght: 400;
+    line-height:24px`,
+    bigBold :`font-family: IBM Plex Sans KR;
+    font-size: 16px;
+    font-weoght: 600;
+    line-height:24px;`,
+    middleRegular :`font-family: IBM Plex Sans KR;
+    font-size: 14px;
+    font-weoght: 400;
+    line-height:20px`,
+    middleBold :`font-family: IBM Plex Sans KR;
+    font-size: 14px;
+    font-weoght: 600;
+    line-height:20px;`,
+    smallRegular :`font-family: IBM Plex Sans KR;
+    font-size: 12px;
+    font-weoght: 400;
+    line-height:18px`,
+    Message :`font-family: IBM Plex Sans KR;
+    font-size: 10px;
+    font-weoght: 400;
+    line-height:16px`,
+}
+const Logos = {
+    bigLogo: `background: url(/assets/images/logos/bigLogo.png) no-repeat;`,
+    smallLogo: `background: url(/assets/images/logos/smallLogo.png) no-repeat;`,
+};
+const Check = {
+    unchecked:  `background: url(/assets/images/icons/unchecked.png) no-repeat;`,
+    checked: `background: url(/assets/images/icons/checked.svg) no-repeat;`
+}
+const Line = {
+    bold: `border: 1px solid #000000;`,
+    regular: `border: 1px solid #7B7B7B;`,
+    light: `border: 1px solid #B7B7B7;`,
+    arrow: `border: 1px solid #D3D3D3;`,
+    dashed: `border: 1px dashed #B7B7B7;`
 }
